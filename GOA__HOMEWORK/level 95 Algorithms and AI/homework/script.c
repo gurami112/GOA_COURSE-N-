cc0fb3d191e58cb7522e782c6ad4a326dde9f33e
gurami123 <gugassmile@gmail.com>
2024-10-26 21:57:57 +0400

    commit

---
+++ GOA__HOMEWORK/level 95 Algorithms and AI/homework/script.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>;
+
+int main( )
+{
+    int space, tab, nl;
+
+    nl = tab = space = 0;
+    int c;
+    
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+            ++space;
+        if (c == '\t')
+            ++tab;
+        if (c == '\n')
+            ++nl;
+    }
+
+    printf ("Blanks %d Tabs %d Lines %d\n", space, tab, nl);
+    return 0;
+}
